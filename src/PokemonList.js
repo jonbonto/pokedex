7d45595b7225d73e533393bb8278af9e3dae82ba
Khalimudin <khalimudin@gmail.com>
2020-03-06 05:36:55 +0700

    list pokemon

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { fetchPokemons } from "./fetchMock";
-import { List, Button, Skeleton } from "antd";
+import { List, Button, Skeleton, Col } from "antd";
 import { useHistory } from "react-router-dom";
 const baseUrl = "https://pokeapi.co/api/v2/";
 const limit = 20;
@@ -14,16 +14,18 @@ export default function PokemonList() {
 
   useEffect(() => {
     const run = async () => {
-      
       setLoading(true);
-      setPokemons([...pokemons, ...[...new Array(limit)].map(() => ({ loading: true }))])
+      setPokemons([
+        ...pokemons,
+        ...[...new Array(limit)].map(() => ({ loading: true }))
+      ]);
       try {
         // const res = await fetch(`${baseUrl}pokemon?limit=${limit}&offset=${lastId}`)
         // const resJson = await res.json();
         const resJson = await fetchPokemons(lastId);
         setPokemons([...pokemons, ...resJson.results]);
         if (!resJson.next) {
-          setGetMore(false)
+          setGetMore(false);
         }
       } catch (error) {}
       setLoading(false);
@@ -32,36 +34,43 @@ export default function PokemonList() {
   }, [setPokemons, setLoading, setGetMore, lastId]);
 
   const onLoadMore = () => {
-    setLastId(id => id + limit)
+    setLastId(id => id + limit);
   };
 
-  const loadMore = getMore && !loading ? (
-    <div
-      style={{
-        textAlign: 'center',
-        marginTop: 12,
-        height: 32,
-        lineHeight: '32px',
-      }}
-    >
-      <Button onClick={onLoadMore}>loading more</Button>
-    </div>
-  ) : null;
+  const loadMore =
+    getMore && !loading ? (
+      <div
+        style={{
+          textAlign: "center",
+          marginTop: 12,
+          height: 32,
+          lineHeight: "32px"
+        }}
+      >
+        <Button onClick={onLoadMore}>loading more</Button>
+      </div>
+    ) : null;
   return (
     <List
       className="demo-loadmore-list"
-      loading={loading}
+      loading={loading && lastId === 0}
       itemLayout="horizontal"
       loadMore={loadMore}
       dataSource={pokemons}
+      grid={{
+        column: 4,
+        gutter: 16
+      }}
+      size="small"
       renderItem={item => (
-        <List.Item
-          onClick={() => push(`/pokemon/${item.name}`, {url: item.url})}
-        >
-          <Skeleton loading={item.loading} active>
-            <div>{item.name}</div>
-          </Skeleton>
-        </List.Item>
+          <List.Item
+            onClick={() => push(`/pokemon/${item.name}`, { url: item.url })}
+            style={{ background: '#fff', border: '1px solid gray'}}
+          >
+            <Skeleton loading={item.loading} active >
+              <div>{item.name}</div>
+            </Skeleton>
+          </List.Item>
       )}
     />
   );
